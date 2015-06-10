cdede40da01566c72bb24025d678c9881510ef3d
Rafał Traciłowski <rafal@tracilowski.pl>
2015-06-10 20:27:35 +0200

    Added command

--- APP/ViewModels/MainWindowViewModel.cs
+++ APP/ViewModels/MainWindowViewModel.cs
@@ -3,13 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Microsoft.Practices.Prism.Commands;
+using Microsoft.Practices.Prism.Mvvm;
 
 namespace APP.ViewModels
 {
-    public class MainWindowViewModel : Microsoft.Practices.Prism.Mvvm.BindableBase
+    public class MainWindowViewModel : BindableBase
     {   
         public string FirstNumber { get; set; }
         public string SecondNumber { get; set; }
         public string Sum { get; set; }
+        public ICommand SumCommand { get; set; } 
+        public MainWindowViewModel()
+        {
+            SumCommand = new DelegateCommand(SumNumbers);
+        }
+        public void SumNumbers()
+        {
+            var first = int.Parse(FirstNumber);
+            var second = int.Parse(SecondNumber);
+            Sum = (first + second).ToString();
+        }
     }
 }
